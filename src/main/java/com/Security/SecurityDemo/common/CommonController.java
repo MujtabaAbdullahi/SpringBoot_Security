d7227766c8121263c751fe12052a2e5e541d540d
DELL <mujtabamuhammadi@gmail.com>
2024-12-22 15:41:06 +0430

    new security configuration

--- src/main/java/com/Security/SecurityDemo/common/CommonController.java
+++ src/main/java/com/Security/SecurityDemo/common/CommonController.java
@@ -1,29 +1,29 @@
 package com.Security.SecurityDemo.common;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @RestController
 public class CommonController {
-    
+
     @GetMapping("/hello")
     public String hello() {
         return "Hollo Dear";
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
     public String getUser() {
+
         return "Hollo User";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String getAdmin() {
+
         return "Hollo Admin";
     }
 
-
-    
 }
